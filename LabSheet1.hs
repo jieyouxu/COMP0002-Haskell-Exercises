b09f6d981b7969c32d4e7f71916ac90852d6864d
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-01 22:55:18 +0100

    Use module for LabSheet1 for QuickCheck
    Additionally, reformat LabSheet1 via hindent

--- LabSheet1.hs
+++ LabSheet1.hs
@@ -1,13 +1,18 @@
 {-|
     Haskell Exercises 1
 
-    Note that functions are implemented with respect to the question 
+    Note that functions are implemented with respect to the question
     requirements, and are not necessarily the most efficient / straightforward
-    implementations possible. 
+    implementations possible.
 -}
+module LabSheet1
+  ( halfEvens
+  , inRange
+  , countPositives
+  ) where
 
-import Data.List
-import Data.Char
+import           Data.Char
+import           Data.List
 
 square :: Int -> Int
 square x = x * x
@@ -17,68 +22,63 @@ pyth x y = (square x) + (square y)
 
 isTriple :: Int -> Int -> Int -> Bool
 isTriple adj opp hyp
-    | adj + opp > hyp = (pyth adj opp) == (square hyp)
-    | otherwise       = False
+  | adj + opp > hyp = (pyth adj opp) == (square hyp)
+  | otherwise = False
 
 isTripleAny :: Int -> Int -> Int -> Bool
-isTripleAny a b c
-    =  (isTriple a b c) 
-    || (isTriple a c b)
-    || (isTriple b a c)
-    || (isTriple b c a)
-    || (isTriple c a b)
-    || (isTriple c b a) 
+isTripleAny a b c =
+  (isTriple a b c) ||
+  (isTriple a c b) ||
+  (isTriple b a c) || (isTriple b c a) || (isTriple c a b) || (isTriple c b a)
 
 isEven :: Int -> Bool
 isEven n = n `mod` 2 == 0
 
 halfIfEven :: Int -> Int
 halfIfEven n
-    | isEven n  = n `div` 2
-    | otherwise = n
+  | isEven n = n `div` 2
+  | otherwise = n
 
 halfEvens :: [Int] -> [Int]
-halfEvens xs =
-    [ halfIfEven n | n <- xs ]
+halfEvens xs = [halfIfEven n | n <- xs]
 
 isInRange :: Int -> Int -> Int -> Bool
 isInRange lowerBound upperBound n
-    | (lowerBound > upperBound) = False
-    | otherwise = (lowerBound <= n) && (n <= upperBound)
+  | (lowerBound > upperBound) = False
+  | otherwise = (lowerBound <= n) && (n <= upperBound)
 
 inRange :: Int -> Int -> [Int] -> [Int]
-inRange lowerBound upperBound source
-    = [ x | x <- source, withinBounds x ]
-    where withinBounds n = isInRange lowerBound upperBound n
+inRange lowerBound upperBound source = [x | x <- source, withinBounds x]
+  where
+    withinBounds n = isInRange lowerBound upperBound n
 
 countPositives :: [Int] -> Int
-countPositives source
-    = length [ n | n <- source, isPositive n ]
-    where isPositive n = n > 0
+countPositives source = length [n | n <- source, isPositive n]
+  where
+    isPositive n = n > 0
 
 capitalised :: String -> String
-capitalised str
-    = (toUpper first) : [ toLower c | c <- rest ]
-    where
-        first = head str
-        rest  = tail str
+capitalised str = (toUpper first) : [toLower c | c <- rest]
+  where
+    first = head str
+    rest = tail str
 
 isLongWord :: String -> Bool
 isLongWord word = (length word) >= 4
 
 capitaliseLongWord :: String -> String
 capitaliseLongWord word
-    | isLongWord word = capitalised word
-    | otherwise       = word
+  | isLongWord word = capitalised word
+  | otherwise = word
 
 stringToLower :: String -> String
-stringToLower str = [ toLower c | c <- str ]
+stringToLower str = [toLower c | c <- str]
 
 title :: [String] -> [String]
 title [] = []
-title words
-    = (capitalised firstWord) : [ capitaliseLongWord w | w <- lowerCaseWords ]
-    where
-        firstWord = head words 
-        rest = tail words
-        lowerCaseWords = [ stringToLower s | s <- rest ]
+title words =
+  (capitalised firstWord) : [capitaliseLongWord w | w <- lowerCaseWords]
+  where
+    firstWord = head words
+    rest = tail words
+    lowerCaseWords = [stringToLower s | s <- rest]
