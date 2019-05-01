c315a282fcfd828aabe77ce6e859243b7a9e518c
jieyouxu <jieyouxu@outlook.com>
2019-05-01 15:50:36 +0100

    Add recursive version of inRange

--- LabSheet2.hs
+++ LabSheet2.hs
@@ -5,3 +5,18 @@
     requirements, and are not necessarily the most efficient / straightforward
     implementations possible. 
 -}
+
+import Data.Char
+
+isInRange :: Int -> Int -> Int -> Bool
+isInRange lowerBound upperBound n
+    = (lowerBound <= n) && (n <= upperBound)
+
+inRange :: Int -> Int -> [Int] -> [Int]
+inRange _ _ [] = []
+inRange lowerBound upperBound (x:xs)
+    | (lowerBound > upperBound) = []
+    | withinBounds x            = x : (inRange lowerBound upperBound xs)
+    | otherwise                 = inRange lowerBound upperBound xs
+    where 
+        withinBounds n = isInRange lowerBound upperBound n
