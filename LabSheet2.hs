2f287513e3d541d62f8f86d208ebf6876cadaeab
jieyouxu <jieyouxu@outlook.com>
2019-05-01 15:40:18 +0100

    Add exercises 2 file

---
+++ LabSheet2.hs
@@ -0,0 +1,7 @@
+{-|
+    Haskell Exercises 2
+
+    Note that functions are implemented with respect to the question 
+    requirements, and are not necessarily the most efficient / straightforward
+    implementations possible. 
+-}
