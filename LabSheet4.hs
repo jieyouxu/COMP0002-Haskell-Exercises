48a6fa6901eb18d4b91f17143c23186e2255c374
jieyouxu <jieyouxu@outlook.com>
2019-05-02 17:20:35 +0100

    Add simple reverse polish notation

--- LabSheet4.hs
+++ LabSheet4.hs
@@ -48,3 +48,46 @@ unwords' :: [String] -> String
 unwords' [] = []
 unwords' [[]] = []
 unwords' words = foldr combineWords [] words
+
+{-
+    Reverse Polish Notation
+
+    Only supports simple expressions of the form "int op int op int ...". No support for operator precendence or parenthesis. By default all four operators are left-associative and are of equal precedence.
+
+    1 + 2 * 3
+
+    Input           Accumulator     Operator
+    [1 + 2 * 3]     []              [ ]
+    [  + 2 * 3]     [1]             [ ]
+    [    2 * 3]     [1]             [+]
+    [      * 3]     [1 2]           [+]
+    [      * 3]     [1 2 +]         [ ]
+    [        3]     [1 2 +]         [*]
+    [         ]     [1 2 + 3]       [*]
+    [         ]     [1 2 + 3 *]     [ ]
+-}
+
+isOperator :: String -> Bool
+isOperator s =  
+    let c = head s in
+        c == '+'
+        || c == '-'
+        || c == '*'
+        || c == '/'
+
+standard2RPNHelper :: [String] -> [String] -> [Char] -> [String]
+standard2RPNHelper [] accumulator [] = accumulator
+standard2RPNHelper [] accumulator (op:[]) = accumulator ++ [[op]]
+standard2RPNHelper (x:[]) accumulator (op:[])
+    = standard2RPNHelper [] (accumulator ++ [x]) [op]
+standard2RPNHelper (x:xs) accumulator []
+    | isOperator x = standard2RPNHelper xs accumulator x
+    | otherwise = standard2RPNHelper xs (accumulator ++ [x]) []
+standard2RPNHelper (x:xs) accumulator (op:[])
+    | isOperator x = standard2RPNHelper xs (accumulator ++ [[op]]) x
+    | otherwise = standard2RPNHelper xs (accumulator ++ [x]) [op]
+
+standard2RPN :: String -> String
+standard2RPN [] = []
+standard2RPN sentence 
+    = unwords' $ standard2RPNHelper (words' sentence) [] [] 
