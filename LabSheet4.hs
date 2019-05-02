df8af93563d7a1d33fa751d4b367c1a24fbb22ec
jieyouxu <jieyouxu@outlook.com>
2019-05-02 17:25:29 +0100

    Break comment at column 80

--- LabSheet4.hs
+++ LabSheet4.hs
@@ -52,7 +52,9 @@ unwords' words = foldr combineWords [] words
 {-
     Reverse Polish Notation
 
-    Only supports simple expressions of the form "int op int op int ...". No support for operator precendence or parenthesis. By default all four operators are left-associative and are of equal precedence.
+    Only supports simple expressions of the form "int op int op int ...". No 
+    support for operator precendence or parenthesis. By default all four 
+    operators are left-associative and are of equal precedence.
 
     1 + 2 * 3
 
