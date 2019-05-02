91b8db0e54165398571fb8298e2df7613ef105da
jieyouxu <jieyouxu@outlook.com>
2019-05-02 15:14:16 +0100

    Add words function
    `words` splits a space-deliminated string into individual words.

---
+++ LabSheet4.hs
@@ -0,0 +1,38 @@
+{-
+    Lab Sheet 4
+-}
+
+import Data.Char
+
+{-
+    Target String: "this is a word"
+    Output                          Original
+    [[]]                            [this is a word]
+    [[t]]                           [his is a word]
+    [[th]]                          [is is a word]
+    [[thi]]                         [s is a word]
+    [[this]]                        [ is a word]
+    [[this], []]                    [is a word]
+    [[this], [i]]                   [s a word]
+    [[this], [is]]                  [ a word]
+    [[this], [is], []]              [a word]
+    [[this], [is], [a]]             [ word]
+    [[this], [is], [a], []]         [word]
+    [[this], [is], [a], [w]]        [ord]
+    [[this], [is], [a], [wo]]       [rd]
+    [[this], [is], [a], [wor]]      [d]
+    [[this], [is], [a], [word]]     []
+-}
+wordsHelper :: String -> [String] -> [String]
+wordsHelper [] accumulator = accumulator
+wordsHelper (c:[]) [[]] = [[c]]
+wordsHelper (c:str) [[]] = wordsHelper str [[c]]
+wordsHelper (c:str) accumulator
+    | isSpace c = wordsHelper str (accumulator ++ [])
+    | otherwise = wordsHelper str (previousWords ++ [newWord])
+    where
+        previousWords = init accumulator
+        newWord = (last accumulator) ++ [c]
+
+words :: String -> [String]
+words str = isSpace c
