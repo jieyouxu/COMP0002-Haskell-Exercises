ac0edfa0f2d7ba1f3de960aae543003e8083401a
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-04 14:13:45 +0100

    Format code

--- LabSheet4.hs
+++ LabSheet4.hs
@@ -1,7 +1,6 @@
 {-
     Lab Sheet 4
 -}
-
 import           Data.Char
 
 {-
@@ -24,15 +23,15 @@ import           Data.Char
     [[this], [is], [a], [word]]     []
 -}
 wordsHelper :: String -> [String] -> [String]
-wordsHelper []        accumulator = accumulator
-wordsHelper (c : [] ) [[]]        = [[c]]
-wordsHelper (c : str) [[]]        = wordsHelper str [[c]]
-wordsHelper (c : str) accumulator
-    | isSpace c = wordsHelper str (accumulator ++ [[]])
-    | otherwise = wordsHelper str (previousWords ++ [newWord])
-    where
-        previousWords = init accumulator
-        newWord       = (last accumulator) ++ [c]
+wordsHelper [] accumulator = accumulator
+wordsHelper (c:[]) [[]] = [[c]]
+wordsHelper (c:str) [[]] = wordsHelper str [[c]]
+wordsHelper (c:str) accumulator
+  | isSpace c = wordsHelper str (accumulator ++ [[]])
+  | otherwise = wordsHelper str (previousWords ++ [newWord])
+  where
+    previousWords = init accumulator
+    newWord = (last accumulator) ++ [c]
 
 words' :: String -> [String]
 words' []       = []
@@ -40,9 +39,9 @@ words' sentence = wordsHelper sentence [[]]
 
 combineWords :: String -> String -> String
 combineWords [] [] = []
-combineWords l  [] = l
+combineWords l []  = l
 combineWords [] r  = r
-combineWords l  r  = l ++ " " ++ r
+combineWords l r   = l ++ " " ++ r
 
 unwords' :: [String] -> String
 unwords' []    = []
@@ -52,8 +51,8 @@ unwords' words = foldr combineWords [] words
 {-
     Reverse Polish Notation
 
-    Only supports simple expressions of the form "int op int op int ...". No 
-    support for operator precendence or parentheses. By default all four 
+    Only supports simple expressions of the form "int op int op int ...". No
+    support for operator precendence or parentheses. By default all four
     operators are left-associative and are of equal precedence.
 
     1 + 2 * 3
@@ -68,26 +67,23 @@ unwords' words = foldr combineWords [] words
     [         ]     [1 2 + 3]       [*]
     [         ]     [1 2 + 3 *]     [ ]
 -}
-
 isOperator :: String -> Bool
-isOperator s = 
-    c == '+' || c == '-' || c == '*' || c == '/'
-    where
-        c = head s
+isOperator s = c == '+' || c == '-' || c == '*' || c == '/'
+  where
+    c = head s
 
 standard2RPNHelper :: [String] -> [String] -> [Char] -> [String]
-standard2RPNHelper [] accumulator []        = accumulator
-standard2RPNHelper [] accumulator (op : []) = accumulator ++ [[op]]
-standard2RPNHelper (x : []) accumulator (op : []) =
-    standard2RPNHelper [] (accumulator ++ [x]) [op]
-standard2RPNHelper (x : xs) accumulator []
-    | isOperator x = standard2RPNHelper xs accumulator x
-    | otherwise    = standard2RPNHelper xs (accumulator ++ [x]) []
-standard2RPNHelper (x : xs) accumulator (op : [])
-    | isOperator x = standard2RPNHelper xs (accumulator ++ [[op]]) x
-    | otherwise    = standard2RPNHelper xs (accumulator ++ [x]) [op]
+standard2RPNHelper [] accumulator [] = accumulator
+standard2RPNHelper [] accumulator (op:[]) = accumulator ++ [[op]]
+standard2RPNHelper (x:[]) accumulator (op:[]) =
+  standard2RPNHelper [] (accumulator ++ [x]) [op]
+standard2RPNHelper (x:xs) accumulator []
+  | isOperator x = standard2RPNHelper xs accumulator x
+  | otherwise = standard2RPNHelper xs (accumulator ++ [x]) []
+standard2RPNHelper (x:xs) accumulator (op:[])
+  | isOperator x = standard2RPNHelper xs (accumulator ++ [[op]]) x
+  | otherwise = standard2RPNHelper xs (accumulator ++ [x]) [op]
 
 standard2RPN :: String -> String
 standard2RPN []       = []
-standard2RPN sentence = 
-    unwords' $ standard2RPNHelper (words' sentence) [] []
+standard2RPN sentence = unwords' $ standard2RPNHelper (words' sentence) [] []
--- LabSheet5.hs
+++ LabSheet5.hs
@@ -2,9 +2,9 @@
 -- post:    fac n = n!
 fac :: Int -> Int
 fac 0 = 1
-fac n 
-    | n < 0     = error "n must be non-negative"     
-    | otherwise = n * (fac (n - 1))
+fac n
+  | n < 0 = error "n must be non-negative"
+  | otherwise = n * (fac (n - 1))
 
 -- pre:     n >= 0
 -- post:    fib n = fibonacci(n)
@@ -12,5 +12,5 @@ fib :: Int -> Int
 fib 0 = 0
 fib 1 = 1
 fib n
-    | n < 0     = error "n must be non-negative"
-    | otherwise = fib (n - 1) + fib (n - 2)
+  | n < 0 = error "n must be non-negative"
+  | otherwise = fib (n - 1) + fib (n - 2)
